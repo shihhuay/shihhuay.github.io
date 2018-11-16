e95ff199c53df6438155f2fd24372aef77e5b999
shihhuay@gmail.com <shihhuay>
2018-11-16 04:46:40 +0000

    colab -> github

--- adl_hw4/index.js
+++ adl_hw4/index.js
@@ -10,7 +10,7 @@ const examples = {
   'example1':
       'To Sherlock Holmes she is always THE woman.',
   'example2':
-      'Alice was beginning to get very tired of sitting by her sister on the bank.',
+      'YOU don't know about me without you have read a book by the name of The Adventures of Tom Sawyer; but that ain't no matter.',
   'example3':
       'You will rejoice to hear that no disaster has accompanied the commencement of an enterprise which you have regarded with such evil forebodings.'      
 };
